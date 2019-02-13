da396fcd2319e2e948bf449a49d42ff787d8864e
Andrew Hoefling <andrew@hoeflingsoftware.com>
2019-02-12 21:30:57 -0500

    Added xamarin config sample

---
+++ XamConfigManager/XamConfigManager/AppSettingsManager.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace XamConfigManager
+{
+    public class AppSettingsManager
+    {
+        private static AppSettingsManager _instance;
+        private JObject _secrets;
+        private const string Namespace = "XamConfigManager";
+        private const string FileName = "appsettings.json";
+
+        private AppSettingsManager()
+        {
+            try
+            {
+                var assembly = IntrospectionExtensions.GetTypeInfo(typeof(AppSettingsManager)).Assembly;
+                var stream = assembly.GetManifestResourceStream($"{Namespace}.{FileName}");
+                using (var reader = new StreamReader(stream))
+                {
+                    var json = reader.ReadToEnd();
+                    _secrets = JObject.Parse(json);
+                }
+            }
+            catch (Exception)
+            {
+                Debug.WriteLine("Unable to load secrets file");
+            }
+        }
+
+        public static AppSettingsManager Settings
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new AppSettingsManager();
+                }
+
+                return _instance;
+            }
+        }
+
+        public string this[string name]
+        {
+            get
+            {
+                try
+                {
+                    var path = name.Split(':');
+
+                    JToken node = _secrets[path[0]];
+                    for (int index = 1; index < path.Length; index++)
+                    {
+                        node = node[path[index]];
+                    }
+
+                    return node.ToString();
+                }
+                catch (Exception)
+                {
+                    Debug.WriteLine($"Unable to retrieve secret '{name}'");
+                    return string.Empty;
+                }
+            }
+        }
+    }
+}
